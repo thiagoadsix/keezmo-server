8a2844d8d925929d4491a36ad7f2fc900b1f51c3
Thiago Andrade Silva <thiago.adsix@gmail.com>
2025-04-30 14:35:43 -0300

    feat(core): add ProgressDynamoRepository and ProgressRepositorySchema with CRUD method stubs

---
+++ src/infrastructure/repository/dynamodb/progress.repository.ts
@@ -0,0 +1,38 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+import { Progress } from "@/domain/entities/progress";
+import { ProgressRepository } from "@/domain/interfaces/repositories";
+
+import { ProgressRepositorySchema } from "@/infrastructure/repository/dynamodb/schemas/progress.repository.schema"
+
+export class ProgressDynamoRepository implements ProgressRepository {
+  constructor(private readonly dynamoDbClient: DynamoDBClient) {}
+
+  async findByCardAndDeck(cardId: string, deckId: string): Promise<Progress | null> {
+    throw new Error("Method not implemented.");
+  }
+
+  async findDueCards(date: Date, deckId?: string): Promise<Progress[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  async save(progress: Progress): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async update(progress: Progress): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async deleteById(id: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async saveBatch(progresses: Progress[]): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async deleteByDeckId(deckId: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+}
---
+++ src/infrastructure/repository/dynamodb/schemas/progress.repository.schema.ts
@@ -0,0 +1,84 @@
+import { AttributeValue } from "@aws-sdk/client-dynamodb"
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb"
+
+import { Progress } from "@/domain/entities/progress"
+
+interface ProgressRepositoryItem {
+  id: string
+  cardId: string
+  deckId: string
+  repetitions: number
+  interval: number
+  easeFactor: number
+  nextReview: string
+  lastReviewed: string
+  createdAt: string
+  updatedAt: string
+}
+
+export class ProgressRepositorySchema implements ProgressRepositoryItem {
+  id: string
+  cardId: string
+  deckId: string
+  repetitions: number
+  interval: number
+  easeFactor: number
+  nextReview: string
+  lastReviewed: string
+  createdAt: string
+  updatedAt: string
+
+  constructor(progress: Progress) {
+    this.id = progress.id
+    this.cardId = progress.cardId
+    this.deckId = progress.deckId
+    this.repetitions = progress.repetitions
+    this.interval = progress.interval
+    this.easeFactor = progress.easeFactor
+    this.nextReview = progress.nextReview
+    this.lastReviewed = progress.lastReviewed
+    this.createdAt = progress.createdAt
+    this.updatedAt = progress.updatedAt
+  }
+
+  static buildPK(deckId: string,) {
+    return `DECK#$#${deckId}`
+  }
+
+  static buildSK(cardId: string) {
+    return `CARD#${cardId}`
+  }
+
+  static buildGSI1PK(cardId: string) {
+    return `CARD#${cardId}`
+  }
+
+  static buildGSI1SK(cardId: string) {
+    return `CARD#${cardId}`
+  }
+
+  toMarshall() {
+    return marshall(this, {
+      convertClassInstanceToMap: true,
+      convertEmptyValues: true,
+      removeUndefinedValues: true,
+    })
+  }
+
+  static fromDynamoItem(item: Record<string, AttributeValue>): Progress {
+    const progress = unmarshall(item)
+
+    return new Progress({
+      id: progress.id,
+      cardId: progress.cardId,
+      deckId: progress.deckId,
+      repetitions: progress.repetitions,
+      interval: progress.interval,
+      easeFactor: progress.easeFactor,
+      nextReview: progress.nextReview,
+      lastReviewed: progress.lastReviewed,
+      createdAt: progress.createdAt,
+      updatedAt: progress.updatedAt,
+    })
+  }
+}
