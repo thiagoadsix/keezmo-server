d5d69d37678fee925e5ee7a7d467f942337e8f4c
Thiago Andrade Silva <thiago.adsix@gmail.com>
2025-05-03 19:33:18 -0300

    feat(config): add serverless configuration for AWS Lambda with createDeck function

---
+++ src/main/serverless.ts
@@ -0,0 +1,41 @@
+import type { AWS } from "@serverless/typescript";
+
+import createDeck from "@/main/endpoints/decks/create";
+
+const serverlessConfiguration: AWS = {
+  service: "keezmo-service",
+  frameworkVersion: "4",
+  plugins: ["serverless-offline"],
+  useDotenv: true,
+  provider: {
+    name: "aws",
+    runtime: "nodejs22.x",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+      DECK_TABLE_NAME: "${env:DECK_TABLE_NAME}",
+    },
+  },
+  functions: {
+    createDeck,
+  },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ["aws-sdk"],
+      target: "node20",
+      define: { "require.resolve": undefined },
+      platform: "node",
+      concurrency: 2,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
